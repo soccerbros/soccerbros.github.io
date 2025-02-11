94dc21b0392b5740088e35be5e0055e93ad6360b
soccerbros <soccerbros779@gmail.com>
2025-02-11 16:01:01 +0500

    Create analytics.js

---
+++ resources/js/analytics.js
@@ -0,0 +1,13 @@
+(function() {
+    var script = document.createElement("script");
+    script.async = true;
+    script.src = "https://www.googletagmanager.com/gtag/js?id=G-TP6188HYPG";
+    document.head.appendChild(script);
+
+    script.onload = function() {
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){ dataLayer.push(arguments); }
+        gtag('js', new Date());
+        gtag('config', 'G-TP6188HYPG');
+    };
+})();
